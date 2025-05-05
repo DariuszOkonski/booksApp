5ae4e4d74a5f966f4ab7f6837897cd57b570a3cd
Dariusz Okoński <darek200180@gmail.com>
2025-05-05 11:23:10 +0200

    solution to exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,37 @@
-
 {
-  'use strict';
+  ('use strict');
 
-  
   const select = {
     templateOf: {
       templateBook: '#template-book',
     },
-    booksList: '.books-list'
+    booksList: '.books-list',
+    booksImages: '.books-list .book__image',
   };
 
   const templates = {
-    templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML)
+    templateBook: Handlebars.compile(
+      document.querySelector(select.templateOf.templateBook).innerHTML
+    ),
   };
-  
+
+  const favoriteBooks = [];
+
+  function initActions() {
+    const booksImages = document.querySelectorAll(select.booksImages);
+    for (const book of booksImages) {
+      book.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        this.classList.add('favorite');
+        favoriteBooks.push(this.getAttribute('data-id'));
+      });
+    }
+  }
+
   function render() {
     const bookList = document.querySelector(select.booksList);
 
-    dataSource.books.forEach(book => {
+    dataSource.books.forEach((book) => {
       const generatedHTML = templates.templateBook(book);
       const generatedListElement = utils.createDOMFromHTML(generatedHTML);
       bookList.appendChild(generatedListElement);
@@ -25,4 +39,5 @@
   }
 
   render();
+  initActions();
 }
